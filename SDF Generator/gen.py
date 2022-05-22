f63a7daee5937e4d4438b7143fd6376ae4a44851
Daniel <daniel.zegers@usm.cl>
2022-05-22 03:33:15 -0400

    Making graph of matrix pre-BFS for SDF

--- SDF Generator/gen.py
+++ SDF Generator/gen.py
@@ -6,41 +6,51 @@ def boundarize(img):
     row, col = img.shape
     bounded_img = np.zeros((row, col))
     for i in range(0, row):
-        if (i != row - 1):
-            for j in range(0, col):
+        for j in range(0, col):
+            if (i == row - 1):
                 if (j != col - 1):
                     if img[i][j] == 0:
-                        if (img[i+1][j] > 0 or img[i][j+1] > 0):
+                        if img[i][j + 1] > 0:
                             bounded_img[i][j] = 128
+                    elif img[i][j + 1] == 0:
+                        bounded_img[i][j] = 128
                     else:
-                        if (img[i+1][j] == 0 or img[i][j+1] == 0):
-                            bounded_img[i][j] = 128
-                        else:
-                            bounded_img[i][j] = 255
-                else:
-                    if img[i][j] == 0:
-                        if (img[i+1][j] > 0):
-                            bounded_img[i][j] = 128
-                    else:
-                        if (img[i+1][j] == 0):
-                            bounded_img[i][j] = 128
-                        else:
-                            bounded_img[i][j] = 255
-        else:
-            for j in range(0, col):
-                if (j != col - 1):
-                    if img[i][j] == 0:
-                        if (img[i][j+1] > 0):
-                            bounded_img[i][j] = 128
-                    else:
-                        if (img[i][j+1] == 0):
-                            bounded_img[i][j] = 128
-                        else:
-                            bounded_img[i][j] = 255
-                else:
-                    if img[i][j] != 0:
                         bounded_img[i][j] = 255
+            elif (j == col - 1):
+                if img[i][j] == 0:
+                    if img[i + 1][j] > 0:
+                        bounded_img[i][j] = 128
+                elif img[i + 1][j] == 0:
+                    bounded_img[i][j] = 128
+                else:
+                    bounded_img[i][j] = 255
+            else:
+                if img[i][j] == 0:
+                    if img[i][j + 1] > 0 or img[i + 1][j] > 0:
+                        bounded_img[i][j] = 128
+                elif img[i][j + 1] == 0 or img[i + 1][j] == 0:
+                    bounded_img[i][j] = 128
+                else:
+                    bounded_img[i][j] = 255
     return bounded_img
+
+def matrix_graph(graph, row, col):
+    cell_no = 0
+    for i in range(0, row):
+        for j in range(0, col):
+            if (i == row - 1):
+                if (j != col - 1):
+                    graph[cell_no].append(cell_no + 1)
+                    graph[cell_no + 1].append(cell_no)
+            elif (j == col - 1):
+                graph[cell_no].append(cell_no + col)
+                graph[cell_no + col].append(cell_no)
+            else:
+                graph[cell_no].append(cell_no + 1)
+                graph[cell_no + 1].append(cell_no)
+                graph[cell_no].append(cell_no + col)
+                graph[cell_no + col].append(cell_no)
+            cell_no += 1
 
 if (len(os.sys.argv) == 3):
     img_dir = os.sys.argv[1]
@@ -59,4 +69,4 @@ if (len(os.sys.argv) == 3):
         cv2.imwrite(fname, bounded_img)
         
 else:
-    print("usage: python gen.py {img_dir}")
+    print("usage: python gen.py {img_dir} {max_dist}")
