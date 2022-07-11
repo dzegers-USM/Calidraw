ad0bb04c13faaccd76263db762751b400d7c3d8d
Daniel Zegers <daniel.zegers@usm.cl>
2022-07-10 20:33:12 -0400

    Add letters only export mode

--- Glyph Exporter/export.py
+++ Glyph Exporter/export.py
@@ -3,10 +3,12 @@ import sys
 import errno
 import fontforge
 
-if (len(os.sys.argv) == 4):
+if (len(os.sys.argv) == 4) or (len(os.sys.argv) == 5):
     font_path = os.sys.argv[1]
     extension = os.sys.argv[2]
-    height = os.sys.argv[3]
+    height = int(os.sys.argv[3])
+    if (len(os.sys.argv) == 5):
+        mode = int(os.sys.argv[4])
     try:
         font = fontforge.open(font_path)
     except OSError as err:
@@ -21,12 +23,24 @@ if (len(os.sys.argv) == 4):
 
     for glyph in font:
         if font[glyph].isWorthOutputting():
-            if font[glyph].glyphname in "abcdefghijklmnopqrstuvwxyz":
-                # Prevents, for ex., "a.png" overwriting "A.png"
-                fpath = "out/" + glyph + "lower." + extension
-            else:
-                fpath = "out/" + glyph + "." + extension
-            font[glyph].export(fpath, int(height) - 1)
+            if (mode == 1):  # Only letters
+                export = False
+                if font[glyph].glyphname in "abcdefghijklmnopqrstuvwxyz":
+                    # Prevents, for ex., "a.png" overwriting "A.png"
+                    fpath = "out/" + glyph + "lower." + extension
+                    export = True
+                elif font[glyph].glyphname in "ABCDEFGHIJKLMNOPQRSTUVWYXZ":
+                    fpath = "out/" + glyph + "." + extension
+                    export = True
+                if export:
+                    font[glyph].export(fpath, height - 1)
+            else:  # Default
+                if font[glyph].glyphname in "abcdefghijklmnopqrstuvwxyz":
+                    # Prevents, for ex., "a.png" overwriting "A.png"
+                    fpath = "out/" + glyph + "lower." + extension
+                else:
+                    fpath = "out/" + glyph + "." + extension
+                font[glyph].export(fpath, height - 1)
 else:
     print("Usage: ffpython export.py {font_path} {extension} {height}")
 sys.exit(0)
